39723037f0a91d3b2c0881f24d909781a2ba5ad4
perezdid <didac.perez@adidas.com>
2023-03-12 17:19:14 +0100

    chapter 7: packages, crates and modules

---
+++ rust/restaurant/src/lib.rs
@@ -0,0 +1,45 @@
+mod front_of_house;
+
+pub fn eat_at_restaurant() {
+    // abs path
+    //crate::front_of_house::hosting::add_to_waitlist();
+
+    // relative path
+    //front_of_house::hosting::add_to_waitlist();
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("wheat");
+    println!("I'd like {} toas please", meal.toast)
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+fn deliver_order() {}
