a1a8196f7358361ebffc1abf768fff1f384d780d
perezdid <didac.perez@adidas.com>
2023-03-17 17:21:59 +0100

    chapter 9: error handling

---
+++ rust/book/errors/src/main.rs
@@ -0,0 +1,67 @@
+use std::fs;
+use std::fs::File;
+use std::io;
+use std::io::ErrorKind;
+use std::io::{self, Read};
+
+fn main() {
+    println!("Hello, world!");
+
+    // panic!("crash and burn");)
+
+    // let v = vec![1, 2, 3];
+    // v[99]; will panic as out of bounds
+
+    let greeting_file_result = File::open("hello.txt");
+    let greeting_file = File::open("hello.txt").unwrap();
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("problem opening the file: {:?}", other_error);
+            }
+        },
+    };
+}
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let username_file_result = File::open("hello.txt");
+
+//     let mut username_file = match username_file_result {
+//         Ok(file) => file,
+//         Err(e) => return Err(e),
+//     };
+
+//     let mut username = String::new();
+
+//     match username_file.read_to_string(&mut username) {
+//         Ok(_) => Ok(username),
+//         Err(e) => Err(e),
+//     }
+// }
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let mut username_file = File::open("hello.txt")?;
+//     let mut username = String::new();
+//     username_file.read_to_string(&mut username)?;
+//     Ok(username)
+// }
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_shorter() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
